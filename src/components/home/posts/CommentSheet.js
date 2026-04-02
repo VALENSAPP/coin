1c899b24caf68ebdf98e0f57bfdaf5b9dfe55f86
SolutionPr <solutions.provider.dev@gmail.com>
2026-04-02 19:14:39 +0530

    fixed image error

--- src/components/home/posts/CommentSheet.js
+++ src/components/home/posts/CommentSheet.js
@@ -8,6 +8,7 @@ import {
   StyleSheet,
   Modal,
   ActivityIndicator,
+  Image,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { formatDistanceToNow } from 'date-fns';
